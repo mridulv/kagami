9fb74325cdb37bc159ebf04b0a671f4ac951d3d8
MRIDUL VERMA <mridul@sumologic.com>
2018-06-03 17:07:11 +0530

    Introduced StringUtils for simple String Manipulation Utilities

---
+++ src/main/scala/com/miuv/util/StringUtils.scala
@@ -0,0 +1,17 @@
+package com.miuv.util
+
+object StringUtils {
+  def randomString(length: Int): String = {
+    val chars = ('a' to 'z') ++ ('A' to 'Z')
+    randomStringFromCharList(length, chars)
+  }
+
+  def randomStringFromCharList(length: Int, chars: Seq[Char]): String = {
+    val sb = new StringBuilder
+    for (i <- 1 to length) {
+      val randomNum = util.Random.nextInt(chars.length)
+      sb.append(chars(randomNum))
+    }
+    sb.toString
+  }
+}
