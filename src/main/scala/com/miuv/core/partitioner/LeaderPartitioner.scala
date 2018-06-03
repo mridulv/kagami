aa52d38fc122d9f1e2d436917139bbcbf4a1f27f
MRIDUL VERMA <mridul@sumologic.com>
2018-06-03 17:01:35 +0530

    Changes to deal with node unavailable tokens for Leader Partitioner

--- src/main/scala/com/miuv/core/partitioner/LeaderPartitioner.scala
+++ src/main/scala/com/miuv/core/partitioner/LeaderPartitioner.scala
@@ -2,10 +2,11 @@ package com.miuv.core.partitioner
 
 import TokenAssigner._
 import com.miuv.core.snapshot.ZookeeperSnapshotMetadataStore
+import com.miuv.util.Logging
 import org.apache.curator.framework.CuratorFramework
 
 class LeaderPartitioner(zookeeperPartitioningStore: ZookeeperPartitioningStore)
-  extends ZookeeperLockUtil {
+  extends ZookeeperLockUtil with Logging {
 
   override val curatorFramework: CuratorFramework = zookeeperPartitioningStore.curatorFramework
   override val lockPath: String = zookeeperPartitioningStore.lockPath
@@ -13,9 +14,22 @@ class LeaderPartitioner(zookeeperPartitioningStore: ZookeeperPartitioningStore)
   def doPartition(currentTargets: Seq[String]): Unit = {
     withLock({
       val currentPartitioning = zookeeperPartitioningStore.load()
-      val underReplicatedTokens = findUnderReplicatedTokens(currentPartitioning, currentTargets)
-      underReplicatedTokens.foreach(replicateUnderReplicatedToken(_, currentPartitioning))
-      zookeeperPartitioningStore.store(currentPartitioning)
+      val partitioningAfterUnavailableNodes = removeUnavailableNodesFromPartitioning(currentPartitioning, currentTargets)
+      val underReplicatedTokens = findUnderReplicatedTokens(partitioningAfterUnavailableNodes)
+      underReplicatedTokens.foreach(replicateUnderReplicatedToken(_, partitioningAfterUnavailableNodes, currentTargets.contains))
+      zookeeperPartitioningStore.store(partitioningAfterUnavailableNodes)
     })
+  }
+
+  private def removeUnavailableNodesFromPartitioning(partitioning: Partitioning, availableNodes: Seq[String]): Partitioning = {
+    val p = partitioning.partitioning
+    val newPartitioning = p.map(tokenWithMetadata => {
+      val token = tokenWithMetadata._1
+      val metadata = tokenWithMetadata._2
+      val secondaryTargets = metadata.secondaryTargets.toSeq.filter(availableNodes.contains).toArray
+      token -> TokenMetadata(metadata.replication, metadata.topic, metadata.primaryTarget, secondaryTargets)
+    })
+    info(s"New Partitioning is: ${newPartitioning} and interesting bit is ${availableNodes}")
+    new Partitioning(newPartitioning)
   }
 }
--- src/main/scala/com/miuv/core/partitioner/TokenAssigner.scala
+++ src/main/scala/com/miuv/core/partitioner/TokenAssigner.scala
@@ -1,39 +1,50 @@
 package com.miuv.core.partitioner
 
 import Partitioning.{Target, Token}
-import TokenAssigner._
 import com.miuv.curator.NodeId
+import com.miuv.util.Logging
 
 class TokenAssigner(nodeId: NodeId, zookeeperPartitioningStore: ZookeeperPartitioningStore)
-extends ClientPartitioner[String, String] {
+  extends ClientPartitioner[String, String] {
+
+  import TokenAssigner._
 
   override def addToken(token: Token, numReplication: Int): Unit = {
     // add self as a target to token
+    info(s"We are going to add a token: $token with replication $numReplication")
     zookeeperPartitioningStore.withLock ({
+      info(s"2 We are going to add a token: $token with replication $numReplication")
       var partitioning = zookeeperPartitioningStore.load()
-      partitioning.addToken(token, TokenMetadata(numReplication, Some(nodeId.nodeName), None, Array.empty))
-      replicateUnderReplicatedToken(token, partitioning)
+      partitioning.addToken(token, TokenMetadata(numReplication, token, Some(nodeId.nodeName), Array.empty))
+      val underReplicatedTokens = findUnderReplicatedTokens(partitioning)
+      info(s"this is while listening new 2 We are having partitioning ${underReplicatedTokens.mkString(",")}")
+      underReplicatedTokens.foreach(replicateUnderReplicatedToken(_, partitioning, _ => true))
       zookeeperPartitioningStore.store(partitioning)
     })
   }
 }
 
-object TokenAssigner {
-  private[partitioner] def replicateUnderReplicatedToken(token: Token, partitioning: Partitioning): Unit = {
+object TokenAssigner extends Logging {
+  private[partitioner] def replicateUnderReplicatedToken(token: Token, partitioning: Partitioning, filterTargetFunc: Target => Boolean): Unit = {
     val metadata = partitioning.partitioning(token)
     (1 to (metadata.replication - metadata.secondaryTargets.length)).foreach(num => {
-      val eligibleTarget = findEligibleTarget(partitioning, token)
-      partitioning.addTargetToToken(token, eligibleTarget)
+      val eligibleTarget = findEligibleTarget(partitioning, token, filterTargetFunc)
+      eligibleTarget.foreach(partitioning.addTargetToToken(token, _))
     })
   }
 
-  private[partitioner] def findEligibleTarget(partitioning: Partitioning, token: Token): Target = {
+  private[partitioner] def findEligibleTarget(partitioning: Partitioning, token: Token, fn: Target => Boolean): Option[Target] = {
     val inversePartitioning = partitioning.inversePartitioning()
     val eligibleTargets = inversePartitioning.filterNot(_._2.contains(token))
-    eligibleTargets.toSeq.minBy(_._2.size)._1
+    val eligibleLiveTargets = eligibleTargets.filter(elem => fn(elem._1))
+    if (eligibleLiveTargets.nonEmpty) {
+      Some(eligibleLiveTargets.toSeq.minBy(_._2.size)._1)
+    } else {
+      None
+    }
   }
 
-  private[partitioner] def findUnderReplicatedTokens(partitioning: Partitioning, currentTargets: Seq[Target]): Seq[Token] = {
+  private[partitioner] def findUnderReplicatedTokens(partitioning: Partitioning): Seq[Token] = {
     partitioning.partitioning.filter(token => token._2.replication > token._2.secondaryTargets.length).keys.toSeq
   }
 }
