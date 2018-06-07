e82381ab4793cf28a3d5b7bb037751fedf65495b
MRIDUL VERMA <mridul@sumologic.com>
2018-06-07 07:06:42 +0530

    Removed Encoding From Interface

--- src/main/scala/com/miuv/core/KagamiClient.scala
+++ src/main/scala/com/miuv/core/KagamiClient.scala
@@ -5,7 +5,5 @@ import com.miuv.core.snapshot.Snapshotter
 
 // Every ServiceInstance has to implement this Client for getting replication requests
 trait KagamiClient extends Snapshotter {
-  type T
-  def replicateRequest(token: Token, request: T)
-  def deserializeRequest(token: Token, request: Array[Byte]): T
+  def replicateData(token: Token, data: Array[Byte])
 }
